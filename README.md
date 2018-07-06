286e4bef7c632d70eb07456676c6388de20287db
Simon Kutuzov <computer016@gmail.com>
2018-07-06 22:17:25 +0300

    First push to origin

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# wallhaven-favorites-downloader
-A Python script that lets you download your entire favorites from wallhaven.cc
+# Wallhaven Favorites Downloader
+
+This script lets you download all your collections into a specified folder. If download folder is not set, the current working directory is used. Each collection is downloaded into its own subfolder with the same name as the collection.
+
+### **Usage**
+python wallhaven_downloader.py [ download_directory ]
+
+### **Example**
+python wallhaven_downloader.py /home/username/Pictures
+
+
+### **Dependencies:**
+* Python >=3.6
+* requests
+* lxml
+* Beautiful Soup 4
---
+++ wallhaven_downloader.py
@@ -0,0 +1,130 @@
+import os
+import sys
+from contextlib import suppress
+
+import requests
+from bs4 import BeautifulSoup
+
+# Control characters used to colorize console output
+COLOR_YELLOW = '\033[93m'
+COLOR_RED = '\033[91m'
+END_COLOR = '\033[0m'
+
+
+def login():
+    login_url = 'https://alpha.wallhaven.cc/auth/login'
+
+    while True:
+        username = input('Username: ')
+        password = input('Password: ')
+        response = requests.post(login_url, data={'username': username, 'password': password})
+
+        if response.url != login_url:
+            print('\nLogin successful.')
+            return response.cookies
+
+        print('Incorrect username/password. Try again.\n')
+
+
+def fetch_filename(wp_id, session=None):
+    '''This function is basically used to determine wallpaper's file extension,
+    which, unfortunately, can't be deduced when fetching wallpaper ids.
+    `session` parameter containing cookies of a successful login is required
+    to run this function on an NSFW wallpaper.
+    '''
+    markup = requests.get(f'https://alpha.wallhaven.cc/wallpaper/{wp_id}', cookies=session).text
+    soup = BeautifulSoup(markup, 'lxml')
+
+    wallpaper_url = soup.find('img', id='wallpaper')['src']
+    return wallpaper_url.rpartition('/')[-1]
+
+
+def fetch_wallpaper_ids(page):
+    thumbnails = page.find('section', class_='thumb-listing-page').ul
+    return [li.figure['data-wallpaper-id'] for li in thumbnails.find_all('li')]
+
+
+def fetch_collections(session):
+    print('Fetching collection ids...\n')
+
+    favs_url = 'https://alpha.wallhaven.cc/favorites'
+    favs_html = requests.get(favs_url, cookies=session).text
+    favs_soup = BeautifulSoup(favs_html, 'lxml')
+    collections = favs_soup.find('ul', class_='blocklist collections-list').find_all('li')
+    del collections[-1]  # drop "trash" collection
+
+    for collection in collections:
+        yield collection['data-collection-id']
+
+
+def download_collection(collection_id, session):
+    collection_url = f'https://alpha.wallhaven.cc/favorites/{collection_id}'
+    wallpaper_base_url = 'https://wallpapers.wallhaven.cc/wallpapers/full/'
+
+    markup = requests.get(collection_url, cookies=session).text
+    soup = BeautifulSoup(markup, 'lxml')
+
+    collection_name = soup.find('header', class_='listing-header collection-header').h1.text
+    num_pages_section = soup.find('header', class_='thumb-listing-page-header')
+
+    # Get wallpaper ids from the first page of the collection
+    print(f'Fetching wallpaper ids for collection "{collection_name}"')
+    wallpaper_ids = fetch_wallpaper_ids(soup)
+
+    if num_pages_section is not None:
+        # Collection contains more than one page, need to fetch the rest
+        num_pages = int(num_pages_section.text.split('/')[-1])
+
+        for page_num in range(2, num_pages + 1):
+            markup = requests.get(collection_url,
+                                  params={'page': page_num},
+                                  cookies=session).text
+            soup = BeautifulSoup(markup, 'lxml')
+            wallpaper_ids.extend(fetch_wallpaper_ids(soup))
+
+    with suppress(FileExistsError):
+        os.mkdir(collection_name)
+    os.chdir(collection_name)
+
+    wallpaper_count = len(wallpaper_ids)
+    for i, wp_id in enumerate(wallpaper_ids):
+        filename = fetch_filename(wp_id, session)
+        response = requests.get(wallpaper_base_url + filename)
+
+        with open(filename, 'wb') as f:
+            f.write(response.content)
+
+        print(f'\rDownloading collection "{collection_name}": {i + 1}/{wallpaper_count}', end='')
+
+    print('\n')
+    os.chdir('..')
+
+
+def main():
+    try:
+        dowload_dir = sys.argv[1]
+    except IndexError:
+        print(f'{COLOR_YELLOW}Warning:{END_COLOR} '
+              'No download folder specified. '
+              'Using current working directory.')
+        dowload_dir = os.getcwd()
+        print(f'Current working directory is {dowload_dir}\n')
+
+    try:
+        os.chdir(dowload_dir)
+    except FileNotFoundError:
+        print(f'{COLOR_RED}Fatal error:{END_COLOR} Download folder not found.')
+        return
+
+    try:
+        session = login()
+        collection_ids = fetch_collections(session)
+
+        for collection in collection_ids:
+            download_collection(collection, session)
+    except requests.ConnectionError:
+        print(f'{COLOR_RED}Fatal error:{END_COLOR} There was a problem with network.')
+
+
+if __name__ == '__main__':
+    main()
